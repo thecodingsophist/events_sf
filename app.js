65ada633a664e102398e87daefb5ce79cfd4e443
Zhanxi Ni <ni.zhanxi@gmail.com>
2018-10-18 16:17:26 -0700

    Added MongoDB and Event model

--- app.js
+++ app.js
@@ -10,13 +10,26 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-let events = [
-  { eventTitle: "Sunset Yoga" },
-  { eventTitle: "Frisbee at Mission Dolores" },
-  { eventTitle: "Corgis Unite!" }
-]
+// let events = [
+//   { eventTitle: "Sunset Yoga" },
+//   { eventTitle: "Frisbee at Mission Dolores" },
+//   { eventTitle: "Corgis Unite!" }
+// ]
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/events_sf');
+
+const Event = mongoose.model('Event', {
+  eventTitle: String
+});
 
 // INDEX
 app.get('/', (req, res) => {
-  res.render('events-index', { events: events });
+    Event.find()
+        .then(events => {
+            res.render('events-index', { events: events });
+        })
+        .catch(err => {
+            console.log(err);
+        })
 })
